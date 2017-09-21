cc63c1dd874c13c7cde3c63f02f01d70813f0cbb
[nabernardo] <nabernardo93@gmail.com>
2017-09-21 15:11:24 -0700

    First commit

---
+++ mario/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace mario
+{
+    class Program
+    {
+        static void Main()
+        {
+            int number;
+            do
+            {
+                Say();
+                string height = Console.ReadLine();
+                Int32.TryParse(height, out number);
+            } while (number < 0 || number > 23);
+        }
+
+        public void Say()
+        {
+            Console.Write("Height:");
+        }
+    }
+}
